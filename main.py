ac25480b4bc60b7aabf0bddb6485c8561826f16b
jagaldol <yoy06056@pusan.ac.kr>
2023-07-30 19:53:09 +0900

    feat: Edit Response structure and request Validation

--- main.py
+++ main.py
@@ -5,32 +5,38 @@ import json
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 @app.post("/api/analyze")
-async def get_image_size(image: UploadFile = File(...), additional_data: str = Form(...)):
+async def get_image_size(image: UploadFile = File(...), f: str = Form(...)):
     try:
         image = Image.open(image.file) # 이미지 객체 받아오기
-        width, height = image.size
+    except Exception:
+        return ErrorResponse(400, "잘못된 이미지입니다.")
+    
+    try:
+        f = float(f)
+    except ValueError:
+        return ErrorResponse(400, "f는 실수 형식이여야 합니다.")
 
-        additional_data_dict = json.loads(additional_data)
-        return {
-            "error": None,
-            "data": {
-                "width": width,
-                "height": height,
-                "additional_data": additional_data_dict
-                }
-            }
-    except Exception as e:
-        return JSONResponse(status_code=500, content=
+    width, height, tall = 1, 2, 3
+
+    return {
+        "status": 200,
+        "response": {
+            "width": width,
+            "height": height,
+            "tall": tall
+            },
+        "errorMessage": None
+        }
+
+def ErrorResponse(status, message):
+    return JSONResponse(status_code=status, content=
                             {
-                                "error": 500,
-                                "data": "서버 에러입니다."
+                                "status": status,
+                                "response": None,
+                                "errorMessage": message
                             })
+    
 
 if __name__ == "__main__":
     import uvicorn
