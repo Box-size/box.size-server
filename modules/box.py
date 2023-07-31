dd082128e5b6b909c56433567884382ef1219760
jihoon5916 <hoon5916@naver.com>
2023-07-31 19:53:26 +0900

    fix: 색 반전 수정

--- modules/box.py
+++ modules/box.py
@@ -9,15 +9,12 @@ def calculate_box_size(image : Image, width : int, height : int, focalLength : f
     
     image = np.array(image)   # image를 cv2에서 사용 가능하게 변환
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    show(image)
 
     box, xyxy = detector.detect(image) #박스 감지하고 crop 리턴
-    show(box)
-
+ 
     box = simplifier.simplify(box) #배경 지우고 외곽선 검출
-    show(box)
 
-    w, h, t = findDot.main(box, image, xyxy)  #findDot.find : 과정 이미지 안뜨는 버전.
+    w, h, t = findDot.find(box, image, xyxy)  #점 찾고 길이 반환   (findDot.main : 과정 이미지 뜨게)
 
     print(w,h,t)
 
@@ -31,6 +28,6 @@ def show(img):
     cv2.imshow("img",img)
     cv2.waitKey()
 
-image = Image.open("modules/images/test5.jpg") # 이미지 테스트
-image.resize((800,600))
-calculate_box_size(image,800,600,1)
+if __name__ == '__main__':
+    image = Image.open("modules/images/test1.jpg") # 이미지 테스트
+    calculate_box_size(image,800,600,1)
