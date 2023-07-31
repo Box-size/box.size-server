7cd0baebfd0873dd13519640f33b204994771e3a
jihoon5916 <hoon5916@naver.com>
2023-07-31 19:25:18 +0900

    fix:detecting 안되던 것 수정

--- modules/box.py
+++ modules/box.py
@@ -8,12 +8,15 @@ import findDot
 def calculate_box_size(image : Image, width : int, height : int, focalLength : float):
     
     image = np.array(image)   # image를 cv2에서 사용 가능하게 변환
-    cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    show(image)
+
     box, xyxy = detector.detect(image) #박스 감지하고 crop 리턴
+    show(box)
 
     box = simplifier.simplify(box) #배경 지우고 외곽선 검출
-    
+    show(box)
+
     w, h, t = findDot.main(box, image, xyxy)  #findDot.find : 과정 이미지 안뜨는 버전.
 
     print(w,h,t)
@@ -28,6 +31,6 @@ def show(img):
     cv2.imshow("img",img)
     cv2.waitKey()
 
-image = Image.open("modules/images/test6.jpg") # 이미지 테스트
+image = Image.open("modules/images/test5.jpg") # 이미지 테스트
 image.resize((800,600))
 calculate_box_size(image,800,600,1)
