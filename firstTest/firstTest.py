a55a56806d1907698d13a478626ee66faf2be9c8
huni00807 <huni00807@gmail.com>
2023-08-04 00:14:09 +0900

    fix:tvec->rvec

--- firstTest/firstTest.py
+++ firstTest/firstTest.py
@@ -28,7 +28,7 @@ def findRT():
         fx, fy = mtx[0][0], mtx[1][1]
         cx, cy = mtx[0][2], mtx[1][2]
         print(dist)
-    return tvec, dist, fx, fy, cx, cy
+    return rvec, dist, fx, fy, cx, cy
 
 def main():
     rvec, tvec = findRT()
